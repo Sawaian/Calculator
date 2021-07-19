0972fecaed633711945aeb121fe52aed356f4a23
Keith <Keith.macario@gmail.com>
2021-07-19 01:28:31 -0700

    I did too much without a commit. It was a bad habit, but I figured out how to evaluate chaining operands. It is very close to being finished.

--- calculator.js
+++ calculator.js
@@ -1,93 +1,19 @@
 
  let currentNumber  = '';
- let a = '';
- let b = '';
+ let num1 = '';
+ let num2 = '';
  let operandUsed = '0';
+ let previousOperand = '' 
+ let equal = '';
+
+ const numBtn = document.querySelectorAll('.numbutton');
+ const operator = document.querySelectorAll('.operator');
+ const clear = document.querySelector('#clear');
 
 
-
-const numBtn = document.querySelectorAll('.numbutton');
- numBtn.forEach(function(numBtn){
-  numBtn.addEventListener('click', function(){
-    currentNumber += this.textContent;
-    if (operandUsed === '0'){
-     a = currentNumber
-    }
-    else{
-      b += currentNumber;
-    }
-   document.getElementById('display').textContent = (currentNumber);
-  });
-});
-
-// Operand buttons that store the operator within the variable operandUsed.
-
-const operator = document.querySelectorAll('.operator');
- operator.forEach(function(operator){
-  operator.addEventListener('click', function(){
-     operandUsed = this.textContent;
-     operatorClicked();
-  });
-});
-
-function operatorClicked(){
-  document.getElementById('display').textContent = (operandUsed);
-  currentNumber = '';
-}
-
-          const equalSign = document.querySelector('#equals');
-              equalSign.addEventListener('click', function(){
-              equals();
-            document.getElementById('display').textContent = (a);
-              b = '0';
-          });
-
-
-    
-
-
-
-
-  function numberThem(){
-    a = parseInt(a);
-    b = parseInt(b);
-  }
-
-// const del = document.querySelector('#delete');
-//     del.addEventListener('click', function(){
-//       currentNumber = currentNumber.slice(0, -1); 
-//     });
-
-
-function equals(){
-       numberThem();
-      if(operandUsed === '+'){
-        a = operation.add(a, b);
-      }
-      if(operandUsed === '-'){
-        a = operation.sub(a, b);
-      }
-      if(operandUsed === '*'){
-       a = operation.multiply(a, b);
-      }
-      if(operandUsed === '/'){
-       a = operation.divide(a, b);
-      }
-    }
-
-function allClear(){
-  a = '';
-  currentNumber = '';
-  operandUsed = '0';
-  document.getElementById('display').textContent = 0;
-
-}
-
-
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', function(){
-  allClear();
-});
+// 1. Object which holds all of the operands. 
+// 2. Operand is checked agaisnt key:pair 
+// 3. A value is returned back to Operation.
 
 
 let operation = {
@@ -95,5 +21,128 @@ let operation = {
   sub: (a, b) => {return a - b},
   multiply: (a, b) => {return a * b},
   divide: (a, b) => {return a / b},
+  }
+
+
+
+// 1. Reads the button input and spits out a number.
+// 2. Checks if the operand is empty. If operand is empty, populate a.
+// 3. If operand is not empmty, populate be. This displays the input. 
+
+ numBtn.forEach(function(numBtn){
+  numBtn.addEventListener('click', function(){
+    currentNumber += this.textContent;
+    numInput();
+  });
+});
+
+function numInput(){
+  if (operandUsed === '0'){
+    num1 = currentNumber
+   }
+   else{
+     num2 = currentNumber;
+   }
+  document.getElementById('display').textContent = (currentNumber);
 }
+
+
+// 1, Operand buttons that store the operator within the variable operandUsed.
+ operator.forEach(function(operator){
+  operator.addEventListener('click', function(){
+     operandUsed = this.textContent;
+     operatorClicked();
+  });
+});
+
+// 1. Displays the operand chosen and changes currentNumber to select B.
+        function operatorClicked(){
+          document.getElementById('display').textContent = (operandUsed);
+          if(equal === '='){
+            calculation();
+          }
+          else
+           { currentNumber = '';
+          }
+          }
+
+//   1. Read the equal sign input. 
+
+          const equalSign = document.querySelector('#equals');
+              equalSign.addEventListener('click', function(){
+                equal = '=';
+                  calculation();
+              });
+
+
+//   1. Checks if either the operand is empty or if b is empty. If yes to either, we give B a's value and calculate. 
+//   2. Second if checks if operand, a, and b are currently not empty. Then it executes the equation with the data 
+// assigned to the variables. Otherwise, if none of the above are true, we clear.
+
+        function calculation(){
+          if(equal === '' && num1 !==''){
+            num2 = num1;
+            equals();
+            result();
+          }
+          if(operandUsed === '' || num2 === ''){
+            num2 = num1;
+            equals();
+            result();
+          }
+          if(operandUsed !=='' && num1 !== '' && num2 !== ''){
+            equals();
+            result();
+          }
+          else{
+            allClear();
+          }
+        }
+
+  
+function result(){
+  document.getElementById('display').textContent = (num1);
+  currentNumber = '';
+  previousOperand = operandUsed
+}
+
+
+
+  function numberThem(){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+  }
+
+
+function equals(){
+       numberThem();
+      if(operandUsed === '+'){
+        num1 = operation.add(num1, num2);
+      }
+      if(operandUsed === '-'){
+        num1 = operation.sub(num1, num2);
+      }
+      if(operandUsed === '*'){
+       num1 = operation.multiply(num1, num2);
+      }
+      if(operandUsed === '/'){
+       num1 = operation.divide(num1, num2);
+      }
+    }
+
+
+function allClear(){
+  num1 = '';
+  num2 = '';
+  currentNumber = '';
+  operandUsed = '0';
+  document.getElementById('display').textContent = 0;
+
+}
+
+
+
+clear.addEventListener('click', function(){
+  allClear();
+});
 
